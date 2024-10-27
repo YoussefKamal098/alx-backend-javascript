5c735eebf7448614b6da0282385326c7b73e55c8
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-27 18:25:48 +0300

    updated 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName)
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
   ]).then((results) => results);
 }

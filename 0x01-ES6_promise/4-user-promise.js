ea5ea2b256cf325c78010dc855051a392709267b
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-27 18:25:13 +0300

    updated 6-final-user.js, 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise.resolve({
-    firstName,
-    lastName,
-  });
+  return new Promise((resolve) => resolve({ firstName, lastName }));
 }

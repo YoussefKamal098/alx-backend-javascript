fe292f7904521af15621b941c57cba20a5d8bf2e
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-27 17:41:39 +0300

    updated 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then((_) => ({
+  return promise.then(() => ({
     status: 200,
     body: 'success',
-  })).catch((_) => Error()).finally(() => {
+  })).catch(() => Error()).finally(() => {
     console.log('Got a response from the API');
   });
 }

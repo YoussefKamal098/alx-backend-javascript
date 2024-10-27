cfeb28be511a4df2a20057c1917590ac27113524
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-27 17:50:16 +0300

    updated 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,9 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    const body = values[0];
-    const { firstName, lastName } = values[1];
-
-    console.log(`${body} ${firstName} ${lastName}`);
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }

a7bfbc764cdd912de48c805573ff04be85478e56
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-27 17:48:27 +0300

    updated 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import {uploadPhoto, createUser} from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return new Promise.all([uploadPhoto(), createUser()]).then((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const body = values[0];
     const { firstName, lastName } = values[1];
 

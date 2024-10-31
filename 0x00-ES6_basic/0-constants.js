160957ff56ed4f71cec80e9ae861d0ea5d14f4c6
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-31 13:11:29 +0300

    updated 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,27 +1,12 @@
-/**
- * Returns a string indicating a preference for using `const`.
- *
- * @returns {string} A message about variable declaration preference.
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
- * Returns a string indicating that something is okay.
- *
- * @returns {string} A message indicating that something is okay.
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * Combines a string about using `let` with a last message.
- *
- * @returns {string} A combination message about variable declaration preferences.
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();

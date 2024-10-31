56e38e4bba2abb24d29e831f98d966428b3b9c34
YoussefKamal098 <kmalywsf098@gmail.com>
2024-10-31 13:10:43 +0300

    updated 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,10 +2,6 @@
  * Returns a string indicating a preference for using `const`.
  *
  * @returns {string} A message about variable declaration preference.
- *
- * @example
- * const message = taskFirst();
- * console.log(message); // Output: "I prefer const when I can."
  */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
@@ -16,10 +12,6 @@ export function taskFirst() {
  * Returns a string indicating that something is okay.
  *
  * @returns {string} A message indicating that something is okay.
- *
- * @example
- * const lastPart = getLast();
- * console.log(lastPart); // Output: " is okay"
  */
 export function getLast() {
   return ' is okay';
@@ -29,12 +21,8 @@ export function getLast() {
  * Combines a string about using `let` with a last message.
  *
  * @returns {string} A combination message about variable declaration preferences.
- *
- * @example
- * const combinationMessage = taskNext();
- * console.log(combinationMessage); // Output: "But sometimes let is okay"
  */
- export function taskNext() {
+export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
